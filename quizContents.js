a0d0ef34a784445b725366a91ad5f3bc3de109c9
Gary Bake <garybake@gmail.com>
2013-08-02 17:33:22 +0100

    Added up to 24 questions

--- quizContents.js
+++ quizContents.js
@@ -1,3 +1,10 @@
+// Amazeballs
+// Biology
+// Famous Cats
+// Historical
+// Jokes
+// Quotations
+
 var questions=new Array();
 var answers=new Array();
 var category=new Array();
@@ -5,7 +12,7 @@ var correct=new Array();
 
 questions[0]="How old was the oldest cat?";
 answers[0]=new Array("29","32","38","40");
-correct[0]=3;
+correct[0]=2;
 category[0]="Amazeballs";
 
 questions[1]="How heavy was the worlds fattest cat?";
@@ -33,32 +40,127 @@ answers[5]=new Array("Red","Brown","Blue","Purrrple");
 correct[5]=3;
 category[5]="Jokes";
 
-questions[6]="Pick 1";
-answers[6]=new Array("1","2","3","4");
+questions[6]="What is he technical term for a cat's hairball?";
+answers[6]=new Array("Bezoar","Capillus","Cortex","Hairy cat sick");
 correct[6]=0;
 category[6]="Biology";
 
-questions[7]="Pick 1";
-answers[7]=new Array("1","2","3","4");
-correct[7]=0;
-category[7]="Quotations";
+questions[7]="What is a group of cats called?";
+answers[7]=new Array("A Posse","A Clowder","A Ground","A Prope");
+correct[7]=1;
+category[7]="Amazeballs";
 
-questions[8]="Pick 1";
-answers[8]=new Array("1","2","3","4");
-correct[8]=0;
-category[8]="Biology";
+questions[8]="Which nation launched the first cat into space?";
+answers[8]=new Array("United Kingdom","USA","Russia","France");
+correct[8]=3;
+category[8]="Historical";
 
-questions[9]="Pick 1";
-answers[9]=new Array("1","2","3","4");
-correct[9]=0;
-category[9]="Quotations";
+questions[9]="What was the first cat in space called?";
+answers[9]=new Array("Space Cat","Felicette","Captain Snuggles","Laika");
+correct[9]=1;
+category[9]="Famous Cats";
 
-questions[10]="Pick 1";
-answers[10]=new Array("1","2","3","4");
-correct[10]=0;
-category[10]="Biology";
+questions[10]="What did the egyptians do when their family cat died?";
+answers[10]=new Array("They shaved their heads","They shaved their eyebrows","Ate the cat","Ate chocolate");
+correct[10]=1;
+category[10]="Historical";
 
-questions[11]="Pick 1";
-answers[11]=new Array("1","2","3","4");
-correct[11]=0;
-category[11]="Quotations";
+questions[11]="What is the largest known litter for a domestic cat?";
+answers[11]=new Array("1","9","19","30");
+correct[11]=2;
+category[11]="Biology";
+
+questions[12]="Which is the earliest ancestor of the modern cat?";
+answers[12]=new Array("Proailurus","Tyrannosaurus","Velociraptor","Mammoth");
+correct[12]=0;
+category[12]="Historical";
+
+questions[13]="What is the name of the Egyptian god with a cat's head?";
+answers[13]=new Array("Osiris","Bast","Ra","Isis");
+correct[13]=1;
+category[13]="Historical";
+
+questions[14]="Which breed of cat is most likely to be cross-eyed?";
+answers[14]=new Array("Siamese","Panther","Persian","Double extra fluffy");
+correct[14]=0;
+category[14]="Amazeballs";
+
+questions[15]="What was the name of the cat in the Alice in wonderland?";
+answers[15]=new Array("Telford Tabby","French Feline","Manchester Moggy","Cheshire Cat");
+correct[15]=0;
+category[15]="Famous Cats";
+
+questions[16]="Who invented the cat flap?";
+answers[16]=new Array("Albert Einstein","Isaac Newton","Leonardo Da Vinci","Winston Churchill");
+correct[16]=1;
+category[16]="Historical";
+
+questions[17]="What percentage of cat owners think their pets can read their minds?";
+answers[17]=new Array("1/3","1/2","None","All");
+correct[17]=0;
+category[17]="Amazeballs";
+
+questions[18]="Why don't cats play poker in the jungle? Too many cheetahs";
+answers[18]=new Array("No gambling laws","Too many cheetahs","Too much rain","Not enough money");
+correct[18]=1;
+category[18]="Jokes";
+
+questions[19]="Where does a cat go when it loses its tail?";
+answers[19]=new Array("The vets","RSCPA","Home","The retail store");
+correct[19]=3;
+category[19]="Jokes";
+
+questions[20]="What do you call a cat that has swallowed a duck?";
+answers[20]=new Array("Fat cat","A duck filled fatty puss","Chubbs","Daffy");
+correct[20]=1;
+category[20]="Jokes";
+
+questions[21]="A cat lover is called...?";
+answers[21]=new Array("Felifile","Kittyfile","Catophile","Ailurophilia");
+correct[21]=3;
+category[21]="Amazeballs";
+
+questions[22]="When was the first cat show?";
+answers[22]=new Array("1604","1874","1880","1902");
+correct[22]=1;
+category[22]="Historical";
+
+questions[23]="How many bones does a cat have?";
+answers[23]=new Array("206","211","223","230");
+correct[23]=3;
+category[23]="Biology";
+
+questions[24]="Who are more likely to own cats?";
+answers[24]=new Array("Men","Women","Men and women equally","Dogs");
+correct[24]=2;
+category[24]="Amazeballs";
+
+questions[25]="xxxx";
+answers[25]=new Array("xxx","xxx","xxx","xxx");
+correct[25]=1;
+category[25]="Jokes";
+
+questions[26]="xxxx";
+answers[26]=new Array("xxx","xxx","xxx","xxx");
+correct[26]=1;
+category[26]="Jokes";
+
+questions[27]="xxxx";
+answers[27]=new Array("xxx","xxx","xxx","xxx");
+correct[27]=1;
+category[27]="Jokes";
+
+questions[28]="xxxx";
+answers[28]=new Array("xxx","xxx","xxx","xxx");
+correct[28]=1;
+category[28]="Jokes";
+
+questions[29]="xxxx";
+answers[29]=new Array("xxx","xxx","xxx","xxx");
+correct[29]=1;
+category[29]="Jokes";
+
+questions[30]="xxxx";
+answers[30]=new Array("xxx","xxx","xxx","xxx");
+correct[30]=1;
+category[30]="Jokes";
